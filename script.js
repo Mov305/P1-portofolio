821ffcfa346cb758b29d53a1e5410616d62fc54c
Mov305 <mov3three2001@gmail.com>
2022-07-19 01:37:37 +0200

    added eslint

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
+/*  eslint linebreak-style: ["error", "windows"]    */
 const menu = document.querySelector('#Union-menu');
 const close = document.querySelector('#nav-button-close');
-const navs = document.querySelector('#menu-navs')
-const links = document.querySelectorAll('#menu-navs li')
+const navs = document.querySelector('#menu-navs');
+const links = document.querySelectorAll('#menu-navs li');
 
 close.addEventListener('click', () => {
-    navs.classList.toggle('hidden');
-})
+  navs.classList.toggle('hidden');
+});
 
 menu.addEventListener('click', () => {
-    navs.classList.toggle('hidden');
-})
+  navs.classList.toggle('hidden');
+});
 
 links.forEach((ele) => ele.addEventListener('click', () => {
-    navs.classList.toggle('hidden')
-}))
-
+  navs.classList.toggle('hidden');
+}));
 
 const bio = document.querySelector('#bio');
 const about = document.querySelector('#about-me');
-const contact = document.querySelector('#contact');
-const activeNavs = document.querySelectorAll('#desk-list > li > a')
+const activeNavs = document.querySelectorAll('#desk-list > li > a');
 
 document.addEventListener('scroll', () => {
-    if (window.pageYOffset < bio.offsetHeight + bio.offsetTop) {
-        activeNavs.forEach(e => e.classList.remove('active'));
-        activeNavs[0].classList.add('active')
-    } else if (window.pageYOffset < about.offsetHeight + about.offsetTop - 200) {
-        activeNavs.forEach(e => e.classList.remove('active'));
-        activeNavs[1].classList.add('active')
-    } else {
-        activeNavs.forEach(e => e.classList.remove('active'));
-        activeNavs[2].classList.add('active')
-    }
-})
+  if (window.pageYOffset < bio.offsetHeight + bio.offsetTop) {
+    activeNavs.forEach((e) => e.classList.remove('active'));
+    activeNavs[0].classList.add('active');
+  } else if (window.pageYOffset < about.offsetHeight + about.offsetTop - 200) {
+    activeNavs.forEach((e) => e.classList.remove('active'));
+    activeNavs[1].classList.add('active');
+  } else {
+    activeNavs.forEach((e) => e.classList.remove('active'));
+    activeNavs[2].classList.add('active');
+  }
+});
